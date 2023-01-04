b44aa2474b3e22da2828fb09ced8b48fd21b8c7e
onjiro <onjiro.mohyahya@gmail.com>
2023-01-04 20:17:02 +0900

    未ログインの場合に403を返すようにした　

--- apps/api/src/routes/accounting/items/index.ts
+++ apps/api/src/routes/accounting/items/index.ts
@@ -2,7 +2,12 @@ import { FastifyPluginAsync } from "fastify"
 
 const items: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
-    return await fastify.prisma.accounting_items.findMany();
+    if (!request.session.user_id) {
+      return reply.status(403).send();
+    }
+
+    const userId = request.session.user_id;
+    return await fastify.prisma.accounting_items.findMany({ where: { user_id: userId } });
   })
 }
 

0e2a49ee55a9eeaa591eec73b3974a0a64a85cf6
onjiro <onjiro.mohyahya@gmail.com>
2023-02-20 22:39:40 +0900

    ログイン用webページを追加

---
+++ apps/web/pages/home/index.tsx
@@ -0,0 +1,3 @@
+export default function Web() {
+  return <h1>home</h1>;
+}
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,41 @@
-import { Button } from "ui";
+import { Col, Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
 export default function Web() {
   return (
-    <div>
-      <h1>Web</h1>
-      <Button />
-    </div>
+    <>
+      <Container className="mt-5">
+        <Row>
+          <Col xs={8} className="bg-light pt-4 pb-4">
+            <h1>家計簿さん</h1>
+            <p>複式簿記で家計簿を！</p>
+            <p>
+              現金や電子マネー、クレジットカードなどの支払いと残高をまとめて管理できます。
+            </p>
+            <p>
+              <Button
+                href="http://localhost:3002/auth/github"
+                variant="primary"
+              >
+                GitHubでログイン
+              </Button>
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <span>
+            &copy;{" "}
+            <a
+              href="https://twitter.com/onjiro_mohyahya"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @onjiro_mohyahya
+            </a>{" "}
+            2013-2023
+          </span>
+        </Row>
+      </Container>
+    </>
   );
 }
